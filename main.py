bffe30c622405c89c28392299cb85becb805c2e6
Tamara Herrington <tamaraherrington24@gmail.com>
2022-06-18 14:25:56 +0100

    create flat matrix

--- main.py
+++ main.py
@@ -1,1 +1,27 @@
-#Python Boggle 3x3 
+# GENERATE 3 X 3 MATRIX (RANDOM LETTERS)
+
+# make this a dict when moving towards creating points per word
+import random
+
+matrix_letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t",
+                  "u", "v", "w", "x", "y", "z"]
+
+# WORD MUST BE >3 <9
+
+m1 = random.choice(matrix_letters)
+m2 = random.choice(matrix_letters)
+m3 = random.choice(matrix_letters)
+m4 = random.choice(matrix_letters)
+m5 = random.choice(matrix_letters)
+m6 = random.choice(matrix_letters)
+m7 = random.choice(matrix_letters)
+m8 = random.choice(matrix_letters)
+m9 = random.choice(matrix_letters)
+
+matrix = [
+    [m1, m2, m3],
+    [m4, m5, m6],
+    [m7, m8, m9]
+    ]
+
+print(matrix)
